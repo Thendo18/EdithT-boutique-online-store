ed7a986fd90b13dc4f11bc57b79b8dd9f056de5b
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-21 14:56:29 +0200

    home page component

--- edith-boutique/src/app/app.module.ts
+++ edith-boutique/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CheckoutPageComponent
   ],
   imports: [
     BrowserModule,
