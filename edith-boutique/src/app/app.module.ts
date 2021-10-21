461ed4d6afe6e2afa7db3b059db0db6771fb7375
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-21 14:59:42 +0200

    cart component

--- edith-boutique/src/app/app.module.ts
+++ edith-boutique/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductsComponent } from './components/products/products.component';
     HomePageComponent,
     CheckoutPageComponent,
     ProductPageComponent,
-    ProductsComponent
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
